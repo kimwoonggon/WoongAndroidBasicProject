f5fa6ca7ab770853cb52bd9be2abc20117cf9805
statking <60002940+kimwoonggon@users.noreply.github.com>
2026-04-04 02:42:09 +0900

    fitXY

---
+++ Twice/app/src/main/java/com/woonggon/twice/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.woonggon.twice
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // 자동이동이면 handler
+        // 클릭이동이면 onClick
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },
+        3000)
+
+
+    }
+    // main으로 넘어가야 한다
+}
