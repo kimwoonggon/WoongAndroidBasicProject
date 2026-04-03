940ad311a180b18fae25c5d3fa8779bd3902c695
statking <60002940+kimwoonggon@users.noreply.github.com>
2026-04-04 04:27:07 +0900

    image change

--- Twice/app/src/main/java/com/woonggon/twice/ImageInsideActivity.kt
+++ Twice/app/src/main/java/com/woonggon/twice/ImageInsideActivity.kt
@@ -1,31 +1,31 @@
 package com.woonggon.twice
-import android.content.Intent
+
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.os.Handler
+// image view load
+// detailimage
+import android.content.Intent
+import android.widget.Toast
+// setimageresource
 
-class SplashActivity : AppCompatActivity() {
+
+class ImageInsideActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_image_inside)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // 자동이동이면 handler
-        // 클릭이동이면 onClick
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        },
-        4000)
+        val imageResId = intent.getIntExtra("imageResId", R.drawable.member_1)
+        findViewById<ImageView>(R.id.detailImage).setImageResource(imageResId)
 
-
+        Toast.makeText(this, "Image Resource ID: $imageResId", Toast.LENGTH_SHORT).show()
     }
-    // main으로 넘어가야 한다
 }
--- Twice/app/src/main/java/com/woonggon/twice/MainActivity.kt
+++ Twice/app/src/main/java/com/woonggon/twice/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.woonggon.twice
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +18,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        fun openImage(imageResId: Int) {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("imageResId", imageResId)
+            startActivity(intent)
+        }
+
+        findViewById<ImageView>(R.id.member1).setOnClickListener { openImage(R.drawable.member_1) }
+        findViewById<ImageView>(R.id.member2).setOnClickListener { openImage(R.drawable.member_2) }
+        findViewById<ImageView>(R.id.member3).setOnClickListener { openImage(R.drawable.member_3) }
+        findViewById<ImageView>(R.id.member4).setOnClickListener { openImage(R.drawable.member_4) }
+        findViewById<ImageView>(R.id.member5).setOnClickListener { openImage(R.drawable.member_5) }
+        findViewById<ImageView>(R.id.member6).setOnClickListener { openImage(R.drawable.member_6) }
+        findViewById<ImageView>(R.id.member7).setOnClickListener { openImage(R.drawable.member_7) }
+        findViewById<ImageView>(R.id.member8).setOnClickListener { openImage(R.drawable.member_8) }
+        findViewById<ImageView>(R.id.member9).setOnClickListener { openImage(R.drawable.member_9) }
 
     }
 }
