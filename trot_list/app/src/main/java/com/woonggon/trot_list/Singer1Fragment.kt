9b3a2d7b7a6baa660dc1e0c230af8925b615bd0b
statking <60002940+kimwoonggon@users.noreply.github.com>
2026-04-06 01:10:12 +0900

    kot file

--- trot_list/app/src/main/java/com/woonggon/trot_list/Singer1Fragment.kt
+++ trot_list/app/src/main/java/com/woonggon/trot_list/Singer1Fragment.kt
@@ -5,28 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Singer1Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Singer1Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,26 +19,19 @@ class Singer1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_singer1, container, false)
+        val view = inflater.inflate(R.layout.fragment_singer1, container, false)
+
+        val image2 = view.findViewById<ImageView>(R.id.image2)
+        image2.setOnClickListener {
+            it.findNavController().navigate(R.id.action_singer1Fragment_to_singer2Fragment)
+                        }
+
+        val image3 = view.findViewById<ImageView>(R.id.image3)
+        image3.setOnClickListener {
+            it.findNavController().navigate(R.id.action_singer1Fragment_to_singer3Fragment)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Singer1Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Singer1Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- trot_list/app/src/main/java/com/woonggon/trot_list/Singer2Fragment.kt
+++ trot_list/app/src/main/java/com/woonggon/trot_list/Singer2Fragment.kt
@@ -5,28 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Singer2Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Singer2Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,26 +21,19 @@ class Singer2Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_singer2, container, false)
+        val view = inflater.inflate(R.layout.fragment_singer2, container, false)
+
+        val image1 = view.findViewById<ImageView>(R.id.image1)
+        image1.setOnClickListener {
+            it.findNavController().navigate(R.id.action_singer2Fragment_to_singer1Fragment)
+        }
+
+        val image3 = view.findViewById<ImageView>(R.id.image3)
+        image3.setOnClickListener {
+            it.findNavController().navigate(R.id.action_singer2Fragment_to_singer3Fragment)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Singer2Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Singer2Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
