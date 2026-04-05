f69816a5bc2b68153a805c9e868a6f65b3b3d3ed
statking <60002940+kimwoonggon@users.noreply.github.com>
2026-04-06 01:09:58 +0900

    image click and show me text

---
+++ trot_list/app/src/main/java/com/woonggon/trot_list/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.woonggon.trot_list
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //1. navigation 만들기
+
+
+        //2. fragment 만들기 연결하기
+            //2.1 이미지 누르면 이동하게 하기
+        //3. 리사이클러뷰 만들기
+    }
+}
