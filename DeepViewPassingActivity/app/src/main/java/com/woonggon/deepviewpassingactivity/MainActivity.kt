6ddade1601c998d2b6b11fd3e6f782cc30b4d63c
statking <60002940+kimwoonggon@users.noreply.github.com>
2026-04-10 23:13:06 +0900

    123

--- DeepViewPassingActivity/app/src/main/java/com/woonggon/deepviewpassingactivity/MainActivity.kt
+++ DeepViewPassingActivity/app/src/main/java/com/woonggon/deepviewpassingactivity/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.woonggon.deepviewpassingactivity
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val REQ_BUTTON = 99
+    val REQ_BUTTON2 = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,43 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+
+        button.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            intent.putExtra("param1", "버튼1에서 신호를 받았습니다")
+            startActivityForResult(intent, REQ_BUTTON)
+        }
+
+        button2.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            intent.putExtra("param2", "버튼2에서 신호를 받았습니다")
+            startActivityForResult(intent, REQ_BUTTON2)
+        }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK)
+        {
+            when (requestCode)
+            {
+                REQ_BUTTON -> {
+                    val returnValue = data?.getStringExtra("param2") ?: "None"
+                    Log.d("액태비티", "돌려받은 값=$returnValue")
+                }
+                REQ_BUTTON2 -> {
+                    val returnValue = data?.getStringExtra("param2") ?: "None"
+                    Log.d("액태비티", "돌려받은 값=$returnValue")
+                }
+            }
+
         }
     }
 }
--- DeepViewPassingActivity/app/src/main/java/com/woonggon/deepviewpassingactivity/SubActivity.kt
+++ DeepViewPassingActivity/app/src/main/java/com/woonggon/deepviewpassingactivity/SubActivity.kt
@@ -1,6 +1,10 @@
 package com.woonggon.deepviewpassingactivity
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,21 @@ class SubActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val btnResult = findViewById<Button>(R.id.btnResult)
+
+        val param = intent.getStringExtra("param1")
+            ?: intent.getStringExtra("param2")
+            ?: "NoSigNAL"
+        textView2.text = param
+
+        btnResult.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("param2", "돌려드립니다")
+            setResult(Activity.RESULT_OK, intent)
+            finish()
         }
     }
 }
